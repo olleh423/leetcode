793ba541da2d307e8a6645fb8af30d024a469f22
Vijay Sharma <olleh423@gmail.com>
2020-10-12 07:33:03 +0530

    code change longest palindrom substring

--- src/problems/LongestPalindromSubstring.java
+++ src/problems/LongestPalindromSubstring.java
@@ -7,18 +7,18 @@ public class LongestPalindromSubstring {
 		if(str == null || str.isEmpty())
 			return null;
 		
-		boolean[][] table = new boolean[str.length()][str.length()];
+		boolean[][] DP = new boolean[str.length()][str.length()];
 		int n = str.length();
 		int maxLength = 1;
 		int startingIndex = 0;
 		
 		for(int i=0 ; i<str.length() ; i++) {
-			table[i][i] = true;
+			DP[i][i] = true;
 		}
 		
 		for(int i=0 ; i<n-1 ; i++) {
 			if(str.charAt(i) == str.charAt(i+1)) {
-				table[i][i+1] = true;
+				DP[i][i+1] = true;
 				startingIndex = i;
 				maxLength = 2;
 			}
@@ -26,20 +26,18 @@ public class LongestPalindromSubstring {
 		
 		for(int k=3 ; k<=n ; k++) {
 			for(int i=0 ; i<n-k+1 ; i++) {
-				int j=i+k-1;
-				if(table[i+1][j-1] && str.charAt(i) == str.charAt(j)) {
-					table[i][j] = true;
-					if(maxLength < k) {
-						startingIndex = i;
-						maxLength = k;
-					}
+				int j = i + k - 1;
+				DP[i][j] = str.charAt(i) == str.charAt(j) && DP[i+1][j-1];
+				if(DP[i][j] && maxLength < k) {
+					maxLength = k;
+					startingIndex = i;
 				}
 			}
 		}
 		
 		return str.substring(startingIndex, startingIndex + maxLength);
 	}
-	
+
 	public static void main(String[] args) {
 		
 		String str = "abcdefgfedcba";
